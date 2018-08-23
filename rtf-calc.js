29fbe0b852e7321bd7e180db5c5fe28c9fa5f344
Neetcord <neetcord@example.com>
2018-08-23 09:41:17 +0900

    Commit

--- rtf-calc.js
+++ rtf-calc.js
@@ -1,13 +1,16 @@
 // (c) Neetcord
 
+var Bluebird = Promise.noConflict();
+
 jQuery(document).ready(function($) {
 
     var serverList = ["명량", "노량", "한산도", "옥계"];
     var rotationList = ["샌디에이고", "타카오", "프린츠오이겐", "후드", "워스파이트", "엔터프라이즈", "일러스트리어스"];
 
-    var serverDiv = $("section.calculation > section.example-answer");
-    var expectingDiv = $("section.calculation > section.expecting-answer");
+    var serverDiv = $("section.example-answer");
+    var expectingDiv = $("section.expecting-answer");
 
+    // Add Calculator DOM
     serverList.forEach(function(s) {
 
         var tempServerDOM
@@ -27,47 +30,62 @@ jQuery(document).ready(function($) {
 
     });
 
-    // Get history
-    $.getJSON("./history/index.json", function (data) {
+    var table = $("table.history-table");
+    var sTH = $("th.result");
+    var sTR = $("tr.servers");
+    var sep = $("td.sep");
+    var trs = [];
+    var log = {};
+    var servers = [];
+    var index = {};
 
-        var table = $("table.history-table");
-        var sTH = $("table.history-table > tr > th.result");
-        var sTR = $("table.history-table > tr.servers");
-        var trs = [];
-        var log = {};
-        var servers = [];
+    // Add History DOM
+    new Bluebird(function (ok) {
 
-        // Fetch data and set server list
-        for (var i = data.start; i <= data.end; i++) {
+        // Get history
+        $.getJSON("./history/index.json", function (data) {
 
-            $.getJSON("./history/" + i + ".json", function (data) {
+            index = data;
 
-                var dataServers = Object.keys(data);
+            var tempC = 0;
 
-                dataServers.forEach(function (s) {
+            // Fetch data and set server list
+            for (var i = data.start; i <= data.end; i++) {
 
-                    if (!servers.includes(s)) {
-                        servers.push(s);
-                        console.log("Addd");
-                        sTR.append("<th>" + s + "</th>");
-                    }
+                $.getJSON("./history/" + i + ".json", function (data) {
+
+                    var dataServers = Object.keys(data);
+
+                    dataServers.forEach(function (s) {
+
+                        if (!servers.includes(s)) {
+                            servers.push(s);
+                            sTR.append("<th>" + s + "</th>");
+                        }
+
+                    });
+
+                    sTH.attr("colspan", servers.length);
+                    sep.attr("colspan", servers.length + 1);
+
+                    log[tempC] = data;
+
+                    if (++tempC > index.end)
+                        ok();
 
                 });
 
-                console.log(data);
-                sTH.attr("colspan", servers.length);
+            }
 
-                log[i] = data;
+        });
 
-            });
-
-        }
+    }).then(function () {
 
         // Create trs
         $.each(log, function (k, v) {
 
             var tempDOM = "<tr>";
-            tempDOM += "<td class='th'>제" + (k + 1) + "회</td>";
+            tempDOM += "<td class='th'>제" + (Number(k) + 1) + "회</td>";
 
             var ss = Object.keys(v);
 
@@ -83,14 +101,23 @@ jQuery(document).ready(function($) {
 
             trs.push(tempDOM);
 
-        })
+        });
+
+        return;
+
+    }).then(function () {
 
         // Append trs
-        for (var i = data.start; i < data.end; i++) {
+        for (var i = index.start; i <= index.end; i++) {
 
             table.append(trs[i]);
 
         }
+
+    }).catch(function (x) {
+
+        console.error("Error occurred while processing rotation histories.");
+        console.debug(x);
 
     });
 
