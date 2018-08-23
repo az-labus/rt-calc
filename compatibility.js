b7d2c879c1bfe0d847d37feb930ccb5c8212265d
Neetcord <neetcord@example.com>
2018-08-23 12:01:05 +0900

    Commit

--- compatibility.js
+++ compatibility.js
@@ -1,11 +1,22 @@
 if (!Array.prototype.includes) {
-  Object.defineProperty(Array.prototype, "includes", {
-    enumerable: false,
-    value: function(obj) {
-        var newArr = this.filter(function(el) {
-          return el == obj;
-        });
-        return newArr.length > 0;
-      }
-  });
+    Object.defineProperty(Array.prototype, "includes", {
+        enumerable: false,
+        value: function(obj) {
+            var newArr = this.filter(function(el) {
+                return el == obj;
+            });
+            return newArr.length > 0;
+        }
+    });
+}
+if (!Object.prototype.values) {
+    Object.prototype.values = function(obj) {
+        var res = [];
+        for (var i in obj) {
+            if (obj.hasOwnProperty(i)) {
+                res.push(obj[i]);
+            }
+        }
+        return res;
+    };
 }
